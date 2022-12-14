a67c646d43ab21eb0cb2841b771e128ec66bb4e1
osmanozyilmaz <osmanyasir.ozyilmaz@iaau.edu.kg>
2022-12-15 02:12:15 +0600

    finished

--- src/components/Image2.js
+++ src/components/Image2.js
@@ -1,32 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import image0 from './assets/kr.jpg';
-import image1 from './assets/kr1.jpg';
-import image2 from './assets/kr2.jpg';
-import image3 from './assets/kr3.jpg';
-import image4 from './assets/kr4.jpg';
-import image5 from './assets/kr5.jpg';
-import image6 from './assets/alatoo logo.jpg';
+import React from 'react'
+import image0 from './assets/kyrgyzclothing01.jpg';
+import image1 from './assets/kyrgyzclothing02.jpg';
+import image2 from'./assets/kyrgyzclothing03.jpg';
+import image3 from './assets/kyrgyzclothing04.jpg';
+import image4 from'./assets/kyrgyzclothing05.jpg';
+import image5 from './assets/kyrgyzclothing06.jpg';
+import image6 from './assets/kyrgyzclothing07.jpg';
 import logo  from './assets/kalpak.webp';
-import '../App.css';
-import Image2 from './Image2';
+import { useState, useEffect  } from 'react';
 
-
-const Image = () => {
+const Image2 = () => {
     const [selectedImage, setSelectedImage] = useState(0)
     const [allImages, setAllImages] = useState([image0, image1, image2, image3, image4, image5])
-    
-
+         
+     
     useEffect(() => {
         setInterval(() => {
             setSelectedImage(selectedImage => selectedImage > 3 ? selectedImage + 1 : 0)
         }, 99999)
     }, [])
-
-    return (
-        <div className='position'>
-            
+     
+         return (
+            <div>
             <h1>KYRGYZSTAN</h1>
-            <p>Click the buttons to change images</p>
+            <p id='clothes'>National Clothes</p>
             <br />
             <img src={allImages[selectedImage]} alt="flag" className='image' />
             <div>
@@ -47,13 +44,11 @@ const Image = () => {
                 </button>
 
             </div>
-            <div>
-            <img src={image6} alt="logo" width={60} className="logo" />
+            <div className='img2'>
+            <a href="#clothes"><img src={logo} alt="logo" width={60} className="logo2"/></a>
             </div>
-            <hr/>
-            <Image2 />
-        </div>
+            </div>
     )
 }
 
-export default Image
+export default Image2
